23a47bf55e114deee2e9bba6ca0ad61113e5d058
SkazuHD <voda0110@gmail.com>
2025-05-25 14:43:17 +0200

    init sqlite

---
+++ lib/data/models/record.dart
@@ -0,0 +1,28 @@
+class Recording {
+  final int id;
+  final String filePath;
+  final int duration;
+  final DateTime createdAt;
+  String? transcription;
+  String? mood;
+
+  Recording({
+    required this.id,
+    required this.filePath,
+    required this.duration,
+    required this.createdAt,
+    this.transcription,
+    this.mood,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'filePath': filePath,
+      'duration': duration,
+      'createdAt': createdAt.toIso8601String(),
+      'transcription': transcription,
+      'mood': mood,
+    };
+  }
+}
