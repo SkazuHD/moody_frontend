48b3bedd118648e492e04a983db1b7285a11c794
Jonas <jonas.wessling@ewetel.net>
2025-05-25 17:36:59 +0200

    Help meeee

---
+++ lib/views/recordList/recordList.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../components/filterList.dart';
+
+class RecordList extends StatelessWidget {
+  const RecordList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: AppBarMoody()),
+      body: Center(child: Column(children: [Expanded(child: FilterList())])),
+    );
+  }
+}
+
+class AppBarMoody extends StatelessWidget {
+  const AppBarMoody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [Text("Image"), Text("SoulLog")],
+    );
+  }
+}
