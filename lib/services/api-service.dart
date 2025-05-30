98d8a9715370f5eaff6de52d82df6d6b77684bc8
jloewnich <jonah-alexander.loewnich@eventim.de>
2025-05-30 12:26:15 +0200

    Generate API

---
+++ lib/services/api-service.dart
@@ -0,0 +1,24 @@
+// Openapi Generator last run: : 2025-05-30T12:15:50.854513
+
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+import 'package:Soullog/api/soullog-api/lib/soullog_api.dart';
+
+
+// To generate files run: dart run build_runner build --delete-conflicting-outputs
+@Openapi(
+  additionalProperties:
+  DioProperties(pubName: 'soullog_api', pubAuthor: 'Soullog'),
+  inputSpec:
+  RemoteSpec(path: 'https://moody-app.skazu.net/openapi.json'),
+ // typeMappings: {'Pet': 'ExamplePet'},
+  generatorName: Generator.dio,
+  runSourceGenOnOutput: true,
+  outputDirectory: 'soullog-api',
+)
+class SoullogApiService {
+  final api = SoullogApi().getDefaultApi();
+
+  SoullogApiService(){}
+
+}
