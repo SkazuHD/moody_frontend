6fd59cb7a9742455287d945634e5bbd8d8879358
Kaja <111892274+kaja1998@users.noreply.github.com>
2025-06-02 17:14:03 +0200

    chartHelper und Home update

--- lib/views/home/chartHelper.dart
+++ lib/views/home/chartHelper.dart
@@ -2,30 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import '../../data/constants/emotions.dart';
 import '../../data/db.dart';
+import '../../data/models/record.dart';
 
 class MoodSpotsNotifier {
-  final ValueNotifier<List<FlSpot>> spots = ValueNotifier<List<FlSpot>>([]);      // Liste der Punkte
-  final ValueNotifier<DateTimeRange> currentRange = ValueNotifier<DateTimeRange>( // Zeitraum: letzte 7 Tage inkl. heute
-    DateTimeRange(
-      start: DateTime.now()
-          .subtract(Duration(days: 6))
-          .copyWith(hour: 0, minute: 0, second: 0, millisecond: 0),
-      end: DateTime.now()
-          .copyWith(hour: 23, minute: 59, second: 59, millisecond: 999),
-    ),
-  );
+  final ValueNotifier<List<FlSpot>> spots = ValueNotifier([]);
 
-  Future<void> fetchAndCalculateSpots() async {   // Einträge aus der Datenbank holen
-    RecordsDB db = await RecordsDB.getInstance();    // Instanz der Datenbank
-
-    // Records der letzten 7 Tage laden
-    final records = await db.getRecordsByDateRange(
-      currentRange.value.start,
-      currentRange.value.end,
+  Future<void> loadSpots({required int pastDays}) async {
+    final now = DateTime.now();
+    final range = DateTimeRange(
+      start: now.subtract(Duration(days: pastDays)),
+      end: now,
     );
 
-    // Einen FlSpot pro Aufnahme erstellen (X = Tag, Y = Emotion-Wert)
-    final calculatedSpots = records.map((record) {    //Transformation geladenen records --> FlSpot-Objekte
+    final db = await RecordsDB.getInstance();
+    final records = await db.getRecordsByDateRange(range.start, range.end);
+
+    spots.value = _calculateSpots(records);
+  }
+
+  List<FlSpot> _calculateSpots(List<Recording> records) {
+    return records.map((record) {
       return FlSpot(
         record.createdAt.day.toDouble(),
         record.mood != null
@@ -37,7 +33,5 @@ class MoodSpotsNotifier {
             : 0.0,
       );
     }).toList();
-
-    spots.value = calculatedSpots;    //ValueNotifier mit den berechneten Punkten aktualisieren
   }
 }
--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -29,7 +29,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    moodSpotsNotifier.fetchAndCalculateSpots();
+    moodSpotsNotifier.loadSpots(pastDays: 7);
   }
 
   @override
