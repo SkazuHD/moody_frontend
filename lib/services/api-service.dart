47feacd3e9a4e73007182f9745b5ca1ebb742e50
SkazuHD <voda0110@gmail.com>
2025-06-02 21:28:26 +0200

    create API Service

--- lib/services/api-service.dart
+++ lib/services/api-service.dart
@@ -1,7 +1,11 @@
 // Openapi Generator last run: : 2025-06-02T17:48:31.142230
-
-import 'package:Soullog/api/soullog-api/lib/src/api.dart';
+import 'package:Soullog/api/soullog-api/lib/soullog_api.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/json_object.dart';
+import 'package:dio/dio.dart';
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+import '../data/models/record.dart';
 
 // To generate files run: dart run build_runner build --delete-conflicting-outputs
 @Openapi(
@@ -16,18 +20,35 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
   outputDirectory: 'lib/api/soullog-api',
 )
 class SoullogApiService {
-  // OPEN QUESTION DO WE NEED A SERVICE
-  // OR DOES THE COMPONENT ITSELF GETS IT LIKE BELOW?
-  final api = SoullogApi().getDefaultApi();
+  final SoullogApi _soullogApi = SoullogApi();
+  final _api = SoullogApi().getDefaultApi();
+  final _serializers = SoullogApi().serializers;
+  final _dio = SoullogApi().dio;
 
   SoullogApiService() {
-    api
-        .rootGet()
-        .then((value) {
-          print(value.data);
-        })
-        .catchError((error) {
-          print("Error: $error");
-        });
+    // Initialize any additional configurations if needed
+  }
+
+  Future<AnalyzeResponse> analyzeRecording(Recording recording) async {
+    if (recording.filePath.isEmpty) {
+      return Future.error("No audio file path provided for analysis.");
+    }
+
+    var personalityBuilder = ListBuilder<JsonObject>();
+    var file = MultipartFile.fromFileSync(recording.filePath);
+    var result = await _api.analyzeAnalyzePost(
+      audio: file,
+      personality: personalityBuilder.build(),
+      cancelToken: CancelToken(),
+      headers: {'Content-Type': 'multipart/form-data'},
+    );
+    if (result.statusCode == 200) {
+      var responseData = result.data;
+      return Future.value(responseData);
+    } else {
+      throw Future.error(
+        'Failed to analyze recording: ${result.statusMessage}',
+      );
+    }
   }
 }
