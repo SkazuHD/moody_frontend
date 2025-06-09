e6ea2e7486fa1392b9146c09a3fcf88940d38f04
SkazuHD <voda0110@gmail.com>
2025-06-09 22:03:07 +0200

    Restrict to portrait mode and update disgust label
    The application is now restricted to portrait mode (up and down) using `SystemChrome.setPreferredOrientations`. Additionally, the label for the `disgusted` emotion has been changed from "Disgusted" to "Disgust".

--- lib/data/constants/emotions.dart
+++ lib/data/constants/emotions.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 enum Emotion {
   angry(emoji: '😠', label: 'Angry', color: Color(0xFFB25A5A), value: 1.0),
-  disgusted(emoji: '🤢', label: 'Disgusted', color: Color(0xFF7A8B3F), value: 2.0),
+  disgusted(emoji: '🤢', label: 'Disgust', color: Color(0xFF7A8B3F), value: 2.0),
   fear(emoji: '😨', label: 'Fear', color: Color(0xFFD9A066), value: 3.0),
   sad(emoji: '😢', label: 'Sad', color: Color(0xFF7A9EAB), value: 4.0),
   neutral(emoji: '😐', label: 'Neutral', color: Color(0xFFB0B7A8), value: 5.0),
